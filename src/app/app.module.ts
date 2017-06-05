c2567c304fd8aa4b1ac07ae1683a25bba92184a0
dmayes403 <davidrmayes@gmail.com>
2017-06-04 22:54:29 -0600

    buttons have been added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AddMovieComponent } from './add-movie/add-movie.component';
+import { AddBandComponent } from './add-band/add-band.component';
+import { AddCarComponent } from './add-car/add-car.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddMovieComponent,
+    AddBandComponent,
+    AddCarComponent
   ],
   imports: [
     BrowserModule
